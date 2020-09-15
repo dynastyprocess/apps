5f4664f2a2d51eee9b85b0cc3e8a799ed8b3aad4
Joe Sydlowski <syd235@gmail.com>
2020-09-14 21:47:47 -0500

    week 1 ep update

--- expectedpoints/app.R
+++ expectedpoints/app.R
@@ -19,7 +19,7 @@ suppressPackageStartupMessages({
 source('../calculator-internal/fn_ui_desktop.R')
 
 setwd(here())
-epdata <- read_parquet("ep_1999_2019.pdata") %>% filter(season >= 2017)
+epdata <- read_parquet("ep_1999_2019.pdata") %>% filter(season >= 2018)
 vars <- epdata %>% select(contains("pass"), contains("rush"), contains("rec"), contains("total")) %>% colnames()
 week_seasons <- epdata %>% arrange(season, week) %>% distinct(week_season) %>% as_vector()
 
@@ -32,14 +32,65 @@ ui <- dashboardPage(
   sidebar_collapsed = FALSE,
   navbar = ui_header(),
   sidebar = ui_sidebar(
-    menuItem('Weekly Breakdowns',tabName = 'weekly',icon = 'chart-line'),
     menuItem('Data Tables',tabName = 'data', icon = 'table'),
+    menuItem('Weekly Breakdowns',tabName = 'weekly',icon = 'chart-line'),
     menuItem('Trends',tabName = 'trends',icon = 'send'),
     menuItem('League Analysis',tabName = 'league',icon = 'trophy'),
     menuItem('About',tabName = 'about',icon = 'question-circle')
   ),
   dashboardBody(
     tabItems(
+      tabItem(tabName = 'data',
+              titlePanel("EP Charts"),
+              box(title = "Inputs",
+                  status = "danger",
+                  width = 12,
+                  fluidRow(
+                    column(width = 5,
+                           radioGroupButtons("selectCol",
+                                             "Select Columns:",
+                                             choices = c("Exp Points","Pass Stats","Rush Stats","Rec Stats","Total Stats","Rate Stats"),
+                                             selected = "Exp Points",
+                                             status = "danger"),
+                           awesomeRadio("weeklyRadio",
+                                        "Data type",
+                                        choices = c("Weekly","Weekly Average","Totals"),
+                                        selected = "Weekly Average",
+                                        status = "danger")
+                           
+                           
+                    ),
+                    column(width = 4,
+                           selectizeInput("selectTeam2",
+                                          "Select Team:",
+                                          choices = c("All", sort(unique(epdata$team))),
+                                          selected = "SEA"),
+                           selectizeInput("selectPos2",
+                                          "Select Position:",
+                                          choices = c("All", "QB", "RB", "WR", "TE"),
+                                          selected = "WR")
+                    ),
+                    column(width = 3,
+                           pickerInput("selectSeason2",
+                                       "Select Season:",
+                                       choices = sort(unique(epdata$season), TRUE),
+                                       selected = "2020",
+                                       multiple = TRUE),
+                           selectizeInput("selectPlayers2",
+                                          "Select Players:",
+                                          choices = c("All"),
+                                          selected = "All",
+                                          multiple = TRUE)
+                    )
+                    
+                  )                    
+              ),
+              box(title = "Table",
+                  width = 12,
+                  fluidRow(width = 12,
+                           DTOutput("table"))
+              )
+      ),
       tabItem(tabName = 'weekly',
               titlePanel('Weekly Breakdowns'),
               box(title = "Inputs",
@@ -61,25 +112,25 @@ ui <- dashboardPage(
                            selectizeInput("selectPos",
                                           "Select Position:",
                                           choices = c("All", "QB", "RB", "WR", "TE"),
-                                          selected = "RB"),
+                                          selected = "RB")
+                    ),
+                    column(width = 4,
+                           pickerInput("selectSeason",
+                                       "Select Season:",
+                                       choices = sort(unique(epdata$season), TRUE),
+                                       selected = "2020",
+                                       multiple = TRUE),
                            selectizeInput("selectPlayers",
                                           "Select Players:",
                                           choices = c("All"),
                                           selected = "All",
                                           multiple = TRUE)
-                    ),
-                    column(width = 4,
-                           selectInput("selectSeason",
-                                       "Select Season:",
-                                       choices = sort(unique(epdata$season), TRUE),
-                                       selected = "2019",
-                                       multiple = TRUE),
-                           sliderTextInput("selectWeeks",
-                                           "Select Week Range:",
-                                           choices = week_seasons,
-                                           selected = week_seasons[c(1,21)],
-                                           width = '150%'
-                           )
+                           # ,sliderTextInput("selectWeeks",
+                           #                 "Select Week Range:",
+                           #                 choices = week_seasons,
+                           #                 selected = week_seasons[c(1,21)],
+                           #                 width = '150%'
+                           # )
                     )
                     
                   )
@@ -94,35 +145,35 @@ ui <- dashboardPage(
                   fluidRow(width = 12,
                            DTOutput("teamPivot"))
               )
-              
       )
     )
   )
 )
 
 
-
 server <- function(input, output, session) {
   
   weeklyEP <- reactive({
-    min_week <- week_master %>% filter(week_season == input$selectWeeks[1]) %>% select(week_season_num)
-    max_week <- week_master %>% filter(week_season == input$selectWeeks[2]) %>% select(week_season_num)
+    # min_week <- week_master %>% filter(week_season == input$selectWeeks[1]) %>% select(week_season_num)
+    # max_week <- week_master %>% filter(week_season == input$selectWeeks[2]) %>% select(week_season_num)
 
     epdata %>%
-      filter(team == input$selectTeam,
-             gsis_pos %in% input$selectPos,
-             season %in% input$selectSeason,
-             between(week_season_num, min_week, max_week))
+      filter((team == input$selectTeam | input$selectTeam == "All"),
+             (gsis_pos %in% input$selectPos | input$selectPos == "All"),
+             season %in% input$selectSeason
+             #,between(week_season_num, min_week, max_week)
+             ) %>%
+      select(season, week, week_season, week_season_num, gsis_id, gsis_name, team, gsis_pos, input$selectVar)
     
   })
   
   weekPivot <- reactive({
     weeklyEP() %>%
       arrange(week_season_num) %>%
+      select(-week_season_num, -week, -season) %>%
       group_by(gsis_id) %>%
-      mutate(Total = sum(.[[input$selectVar]], na.rm = TRUE),
-             Average = mean(.[[input$selectVar]], na.rm = TRUE)) %>%
-      select(gsis_id, gsis_name, team, gsis_pos, week_season, input$selectVar, Total, Average) %>%
+      mutate(Total = sum(.data[[input$selectVar]], na.rm = TRUE),
+             Average = mean(.data[[input$selectVar]], na.rm = TRUE)) %>%
       pivot_wider(names_from = week_season,
                   values_from = input$selectVar) %>%
       ungroup() %>%
@@ -131,47 +182,50 @@ server <- function(input, output, session) {
       arrange(desc(Average))
   })
   
-  observeEvent({input$selectSeason},{
-    week_seasons_reactive <-
-      epdata %>%
-      filter(season %in% input$selectSeason) %>%
-      arrange(season, week) %>%
-      distinct(week_season) %>%
-      as_vector()
-    
-    updateSliderTextInput(
-      session = session,
-      inputId = "selectWeeks",
-      choices = as.character(week_seasons_reactive)
-    )
-  })
+  # observeEvent({input$selectSeason},{
+  #   week_seasons_reactive <-
+  #     epdata %>%
+  #     filter(season %in% input$selectSeason) %>%
+  #     arrange(season, week) %>%
+  #     distinct(week_season) %>%
+  #     as_vector()
+  #   
+  #   updateSliderTextInput(
+  #     session = session,
+  #     inputId = "selectWeeks",
+  #     choices = as.character(week_seasons_reactive)
+  #   )
+  # })
   
   output$pivotGraph <- renderPlot({
-
-    weeklyEP() %>%
+    pivotgraph_data <<- weeklyEP() %>%
       group_by(gsis_id) %>%
       mutate(player_weeks = n()) %>%
       ungroup() %>%
       group_by(season, week, week_season) %>%
-      #arrange(season, week) %>%
       mutate(week_season_num = cur_group_id(),
              week_season_num = case_when(player_weeks > 2 ~ week_season_num,
                                          TRUE ~ 0L)) %>%
       ungroup() %>%
-      mutate(week_season = fct_reorder(week_season, week_season_num)) %>%
-      #arrange(week_season_num) %>%
-      #mutate(Week = factor())
+      mutate(week_season = fct_reorder(week_season, week_season_num))
+    
+    plot_breaks <- unique(pivotgraph_data$week_season)
+    
+    pivotgraph_data %>% 
       ggplot(aes(x = week_season, y = .data[[input$selectVar]], color = gsis_name)) +
       geom_point(size = 3) +
       theme_bw() + 
       labs(x="Week", y=input$selectVar, title=paste0("Weekly Summary \n",input$selectVar,": ",input$selectTeam," | ",input$selectPos)) +
       theme(plot.title = element_text(face='bold'),
-            #axis.text.x = element_text(angle=45, hjust=1),
+            panel.spacing = unit(0,"lines"),
             text = element_text(size=18)) +
-      {if(max(.data$week_season_num) < 20) scale_x_discrete(guide = guide_axis(angle = 45))
-        else if(max(.data$week_season_num) > 20) scale_x_discrete(guide = guide_axis(n.dodge = 2))} + 
-      if(input$pivot_trendlines){geom_smooth(aes(x=week_season_num, y=.data[[input$selectVar]]),
-                                             method = "loess", na.rm = TRUE, fill = NA)}
+      scale_x_discrete(breaks = plot_breaks[seq(1, by = case_when(length(plot_breaks) < 22 ~ 2,
+                                                                  length(plot_breaks) < 44 ~ 4,
+                                                                  length(plot_breaks) < 66 ~ 8,
+                                                                  TRUE ~ 16),
+                                                to = length(plot_breaks))]) +
+      if(input$pivot_trendlines){geom_smooth(aes(x=week_season_num, y = pivotgraph_data[[input$selectVar]]),
+                                             method = "loess", na.rm = TRUE, fill = NA)} 
   })
   
   output$teamPivot <- renderDT({
@@ -179,14 +233,62 @@ server <- function(input, output, session) {
     
     weekPivot() %>% 
       datatable(
+        class = "compact stripe nowrap",
         rownames=T,
         options(
           scrollX=TRUE,
           paging=FALSE,
-          searching=FALSE)) #%>%
-      #formatRound(columns = c(2:ncol(selCols())), digits = if (grepl('share',input$selectVar)) {3} else {1})
+          searching=FALSE))
   })
   
+  weeklyEP2 <- reactive({
+    field_names <- switch(input$selectCol,
+                          "Exp Points" = rlang::exprs(ends_with("x")),
+                          "Pass Stats" = rlang::exprs(starts_with("pass")),
+                          "Rush Stats" = rlang::exprs(starts_with("rush")),
+                          "Rec Stats" = rlang::exprs(starts_with("rec")),
+                          "Total Stats" = rlang::exprs(starts_with("total")))
+    
+    arrange_field <- switch(input$selectCol,
+                          "Exp Points" = rlang::exprs(total_fp_x),
+                          "Pass Stats" =  rlang::exprs(pass_fp_x),
+                          "Rush Stats" =  rlang::exprs(rush_fp_x),
+                          "Rec Stats" =  rlang::exprs(rec_fp_x),
+                          "Total Stats" =  rlang::exprs(total_fp_x))
+    
+    epdata %>%
+      filter((team == input$selectTeam2 | input$selectTeam2 == "All"),
+             (gsis_pos %in% input$selectPos2 | input$selectPos2 == "All"),
+             season %in% input$selectSeason2) %>%
+      select(season, week, gsis_name, team, gsis_pos, !!!field_names, -contains("_team")) %>%
+      {if (input$weeklyRadio == "Weekly Average")
+        group_by(., gsis_name, team, gsis_pos) %>%
+          summarise(games = n(),
+                    across(!!!field_names, ~mean(.x, na.rm = TRUE))) 
+        else if (input$weeklyRadio == "Totals")
+          group_by(., gsis_name, team, gsis_pos) %>%
+          summarise(games = n(),
+                    across(!!!field_names, ~sum(.x, na.rm = TRUE))) 
+        
+        else .} %>% 
+      ungroup() %>% 
+      mutate(across(where(is.numeric), ~round(.x, 1))) %>% 
+      arrange(-(!!!arrange_field))
+    
+  })
+  
+  output$table <- renderDT({
+    #req(input$weeklyRadio == "Weekly")
+    
+    weeklyEP2() %>% 
+      datatable(
+        class = "compact stripe nowrap",
+        rownames=T,
+        options(
+          scrollX=TRUE,
+          paging=FALSE,
+          searching=FALSE))
+  })
 }
 
 shinyApp(ui, server)
