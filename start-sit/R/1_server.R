9dc42633573f429e5a59b2501dc957b57ebedfec
Joe Sydlowski <syd235@gmail.com>
2021-09-10 01:06:33 -0500

    combine app folders, split out functions

---
+++ start-sit/R/1_server.R
@@ -0,0 +1,128 @@
+# SETUP  ----------------------------------------------------------
+
+suppressPackageStartupMessages({
+  library(ffscrapr)
+  library(ffpros)
+  library(ffsimulator)
+  library(tidyverse)
+  library(gt)
+  library(RColorBrewer)
+  
+  # Shiny
+  library(shiny)
+  library(bs4Dash)
+  library(shinyWidgets)
+  
+})
+
+
+# platform <- "fleaflicker"
+# league_id <- 312861
+# conn_obj <- load_conn(platform, league_id)
+# roster_obj <- load_rosters(platform, league_id, conn_obj)
+# roster_combo <- combine_sources(conn_obj, platform)
+
+load_conn <- function(platform, league_id){
+  ff_connect(platform = platform, league_id = league_id, season = 2021)
+}
+
+get_projections_page <- function(pos){
+  fp_projections(page = pos, sport = "nfl", year = 2021, week = 1)
+}
+
+
+get_projections <- function(){
+  tibble(position = c("qb","rb","wr","te")) %>% 
+    mutate(projections = map(position, get_projections_page),
+           position = str_to_upper(position)) %>% 
+    unnest(projections) %>% 
+    pivot_longer(cols = where(is.numeric))
+}
+
+load_projections <- function(platform, conn_obj){
+  
+  # Pull Scoring Rules
+  if (platform == "fleaflicker") {
+    league_scoring <- ff_scoring(conn_obj) %>% transmute(pos, points, event = as.character(event_id))
+  } else {league_scoring <- ff_scoring(conn_obj)}
+  
+  #Clean scoring rules
+  stat_mapping <-
+    ffscrapr::nflfastr_stat_mapping %>%
+    mutate(stat_name = case_when(nflfastr_event == "passing_yards" ~ "passing_yds",
+                                 nflfastr_event == "completions" ~ "passing_cmp",
+                                 nflfastr_event == "attempts" ~ "passing_att",
+                                 nflfastr_event == "interceptions" ~ "passing_ints",
+                                 nflfastr_event == "carries" ~ "rushing_att",
+                                 nflfastr_event == "rushing_yards" ~ "rushing_yds",
+                                 str_detect(nflfastr_event, "fumbles_lost")  ~ "misc_fl",
+                                 nflfastr_event == "receptions" ~ "receiving_rec",
+                                 nflfastr_event == "receiving_yards" ~ "receiving_yds",
+                                 TRUE ~ nflfastr_event)) %>% 
+    filter(platform == platform) %>% 
+    select(-nflfastr_event) %>% 
+    distinct()
+  
+  #Apply rules to each roster
+  get_projections() %>% 
+    inner_join(stat_mapping, by = c("name"="stat_name"), na_matches ="never") %>% 
+    left_join(league_scoring, by = c("ff_event"="event", "position"="pos"), na_matches ="never") %>% 
+    mutate(projected_points = value*points) %>% 
+    group_by(position, fantasypros_id, player_name, team) %>% 
+    summarise(projected_points = sum(projected_points, na.rm = TRUE))
+  
+}
+
+load_rankings <- function(conn_obj){
+  
+  league_settings <- ff_league(conn_obj)
+  
+  if(league_settings$qb_type == "2QB/SF" & str_detect(league_settings$scoring_flags,"1_ppr")) {
+    fp_rankings(page = "ppr-superflex", sport = "nfl")
+  } else if (league_settings$qb_type == "2QB/SF") {
+    fp_rankings(page = "superflex", sport = "nfl")
+  } else if (str_detect(league_settings$scoring_flags,"1_ppr")) {
+    fp_rankings(page = "ppr-flex", sport = "nfl") %>% 
+      bind_rows(fp_rankings(page = "qb", sport = "nfl"))
+  } else {fp_rankings(page = "flex", sport = "nfl") %>% 
+      bind_rows(fp_rankings(page = "qb", sport = "nfl"))}
+  
+}
+
+injury_data <- function(){
+  nflreadr::load_injuries(2021) %>%
+    left_join(select(dp_playerids(), gsis_id, fantasypros_id), by = "gsis_id") %>% 
+    select(fantasypros_id, practice_status, report_status)
+}
+
+combine_sources <- function(conn_obj, platform){
+  
+  ffs_rosters(conn_obj) %>%
+    full_join(load_projections(platform, conn_obj),
+              by = c("fantasypros_id", "pos" = "position"),
+              suffix = c(".rosters", ".proj")) %>% 
+    full_join(load_rankings(conn_obj),
+              by = c("fantasypros_id", "pos"),
+              suffix = c("", ".ranks")) %>%
+    left_join(injury_data(), by = "fantasypros_id", na_matches ="never") %>% 
+    transmute(franchise_name = replace_na(franchise_name, "Free Agent"),
+              player_name = coalesce(player_name.rosters, player_name.proj, player_name),
+              # player_id,
+              fantasypros_id,
+              # sportradar_id = coalesce(sportradar_id, sportradar_id.ranks),
+              position = factor(pos, levels = c("QB","RB","WR","TE"), ordered = TRUE),
+              team = coalesce(team.rosters, team.proj, team),
+              player_image_url,
+              ovr_rank = rank,
+              pos_rank,
+              ecr,
+              best,
+              worst,
+              projected_points = replace_na(projected_points, 0),
+              practice_status,
+              report_status)
+  
+}
+
+
+
--- start-sit/app.R
+++ start-sit/app.R
@@ -3,6 +3,7 @@
 suppressPackageStartupMessages({
   library(ffscrapr)
   library(ffpros)
+  library(ffsimulator)
   library(tidyverse)
   library(gt)
   library(RColorBrewer)
@@ -18,7 +19,6 @@ options(warn=0, dplyr.summarise.inform = FALSE)
 
 # DATA PREP --------------------------------------------------------------
 
-get_projections <- function(pos){fp_projections(page = pos, sport = "nfl", year = 2021, week = 1)}
 
 # UI section --------------------------------------------------------------
 ui <- dashboardPage(
@@ -40,27 +40,25 @@ ui <- dashboardPage(
                                           "Select Playform:",
                                           choices = c("mfl","sleeper","fleaflicker","espn"),
                                           multiple = FALSE,
-                                          selected = NULL)
+                                          selected = "fleaflicker")
                     ),
                     column(width = 4,
-                           textInput("league_id", label = "Enter League ID")
+                           textInput("league_id", label = "Enter League ID", value = 312861)
+                           # actionButton("load_data",
+                           #              "Load League",
+                           #              icon = icon("list-ol"),
+                           #              class = "btn-success")
                     ),
                     column(width = 4,
                            selectizeInput("select_team",
                                           "Select Team:",
                                           choices = c(),
                                           multiple = FALSE,
-                                          selected = NULL)
-                    )
-                  )
-              )
-      )
-    ),
+                                          selected = NULL)))))),
     box(
       width = 12,
       fluidRow(width = 12,
-               gt_output("team_table")))
-  )
+               gt_output("team_table"))))
 )
 
 
@@ -68,90 +66,25 @@ ui <- dashboardPage(
 
 server <- function(input, output, session) {
   
-  league_conn <- reactive({ ff_connect(platform = input$select_platform, league_id = input$league_id, season = 2021) })
   
-  league_settings <- reactive({ ff_league(league_conn()) })
+  league_conn <- reactiveVal()
+  rosters <- reactiveVal()
   
-  league_franchises <-
-    reactive({
-      req(input$league_id)
-      ff_franchises(league_conn()) })
+  #react on enter button
+  observeEvent({input$league_id},{
+    league_conn(load_conn(input$select_platform, input$league_id))
+    
+    rosters(combine_sources(league_conn(), input$select_platform))
+    
+    })
   
   observeEvent({input$league_id},{
     updateSelectizeInput(session, 'select_team',
-                         choices = league_franchises()$franchise_name,
-                         selected = league_franchises() %>% sample_n(1) %>% pull(franchise_name))
+                         choices = rosters() %>% pull(franchise_name) %>% distinct(),
+                         selected = rosters() %>% sample_n(1) %>% pull(franchise_name))
+    
   })
-  
-  projections_df <- 
-    reactive({
-      tibble(position = c("qb","rb","wr","te")) %>% 
-        mutate(projections = map(position, get_projections),
-               position = str_to_upper(position)) %>% 
-        unnest(projections) %>% 
-        pivot_longer(cols = where(is.numeric))
-    })
-  
-  stat_mapping <-
-    reactive({
-      ffscrapr::nflfastr_stat_mapping %>%
-        mutate(stat_name = case_when(nflfastr_event == "passing_yards" ~ "passing_yds",
-                                     nflfastr_event == "completions" ~ "passing_cmp",
-                                     nflfastr_event == "attempts" ~ "passing_att",
-                                     nflfastr_event == "interceptions" ~ "passing_ints",
-                                     nflfastr_event == "carries" ~ "rushing_att",
-                                     nflfastr_event == "rushing_yards" ~ "rushing_yds",
-                                     str_detect(nflfastr_event, "fumbles_lost")  ~ "misc_fl",
-                                     nflfastr_event == "receptions" ~ "receiving_rec",
-                                     nflfastr_event == "receiving_yards" ~ "receiving_yds",
-                                     TRUE ~ nflfastr_event)) %>% 
-        filter(platform == "sleeper") %>% 
-        select(-nflfastr_event) %>% 
-        distinct()
-    })
 
-  join_rules <- 
-    reactive({
-      projections_df() %>% 
-        inner_join(stat_mapping(), by = c("name"="stat_name"), na_matches ="never") %>% 
-        left_join(ff_scoring(league_conn()), by = c("ff_event"="event","position"="pos"), na_matches ="never") %>% 
-        mutate(projected_points = value*points) %>% 
-        group_by(position, fantasypros_id, player_name, team) %>% 
-        summarise(projected_points = sum(projected_points, na.rm = TRUE))
-    })
-  
-  rankings_df <- 
-    reactive({
-      if(league_settings()$qb_type == "2QB/SF" & str_detect(league_settings()$scoring_flags,"1_ppr")) {
-        fp_rankings(page = "ppr-superflex", sport = "nfl")
-      } else if (league_settings()$qb_type == "2QB/SF") {
-        fp_rankings(page = "superflex", sport = "nfl")
-      } else if (str_detect(league_settings()$scoring_flags,"1_ppr")) {
-        fp_rankings(page = "ppr-flex", sport = "nfl") %>% 
-          bind_rows(fp_rankings(page = "qb", sport = "nfl"))
-      } else {fp_rankings(page = "flex", sport = "nfl") %>% 
-          bind_rows(fp_rankings(page = "qb", sport = "nfl"))}
-    })
-  
-  rosters <-
-    reactive({
-      rankings_df() %>% 
-        left_join(select(ffscrapr::dp_playerids(), fantasypros_id, sleeper_id), by = "fantasypros_id") %>%
-        left_join(join_rules(), by = "fantasypros_id") %>% 
-        left_join(ff_rosters(league_conn()), by = c("sleeper_id"="player_id")) %>% 
-        transmute(franchise_name,
-                  player_name,
-                  position = factor(position, levels = c("QB","RB","WR","TE"), ordered = TRUE),
-                  team,
-                  rank,
-                  ecr,
-                  best,
-                  worst,
-                  projected_points)
-    })
-  
-  # league_starters <- ff_starter_positions(league_conn)
-  
   output$team_table <- render_gt({
     req(input$league_id)
     
@@ -164,13 +97,13 @@ server <- function(input, output, session) {
                  player_name = "Player",
                  position = "Position",
                  team = "Team",
-                 rank = "Position",
+                 ovr_rank = "Overall",
                  ecr = "Consensus",
                  best = "Best",
                  worst = "Worst",
                  projected_points = "Projected Points") %>% 
       tab_spanner(label = "Rank",
-                  columns = c(rank, ecr, best, worst)) %>% 
+                  columns = c(ovr_rank, ecr, best, worst)) %>% 
       data_color(
         columns = c(projected_points),
         colors = scales::col_factor(
